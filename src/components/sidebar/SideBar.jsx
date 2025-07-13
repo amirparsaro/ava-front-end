c50d506e6819d46b6fa41421a6787ba6ce40aa59
amirparsaro <amirparsarouhani@gmail.com>
2025-07-13 21:12:44 +0330

    create component sidebar

---
+++ src/components/sidebar/SideBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.css';
+
+const SideBar = () => {
+    return (
+        <aside className="sidebar">
+            
+        </aside>
+    );
+};
+
+export default SideBar; 
