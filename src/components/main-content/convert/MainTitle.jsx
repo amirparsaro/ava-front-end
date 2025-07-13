2b125d8415f63f28e39438a6321439e91ebf1814
amirparsaro <amirparsarouhani@gmail.com>
2025-07-14 00:30:08 +0330

    create component mainTitle

---
+++ src/components/main-content/convert/MainTitle.jsx
@@ -0,0 +1,15 @@
+import "../../../App.css";
+
+const MainTitle = () => {
+  return (
+    <div class="main-title-container">
+      <h1>تبدیل گفتار به متن</h1>
+      <p>
+        آوا با استفاده از هزاران ساعت گفتار با صدای افراد مختلف، زبان فارسی را
+        یاد گرفته است و می‌تواند متن صحبت‌ها را بنویسد.
+      </p>
+    </div>
+  );
+};
+
+export default MainTitle;
