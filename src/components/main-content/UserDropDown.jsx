aed334edc0f5194086f4650349566a44f4299f7f
amirparsaro <amirparsarouhani@gmail.com>
2025-07-14 14:44:56 +0330

    create component option

--- src/components/main-content/UserDropDown.jsx
+++ src/components/main-content/UserDropDown.jsx
@@ -8,20 +8,20 @@ const UserDropDown = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div class="header">
+    <div className="header">
       <button
         className={isOpen ? "user-button open" : "user-button"}
         onClick={() => setIsOpen(!isOpen)}
       >
-        <div class="user-title">
-          <img src={UserIcon} alt="user-icon" class="user-icon"></img>
+        <div className="user-title">
+          <img src={UserIcon} alt="user-icon" className="user-icon"></img>
           <p>مهمان</p>
-          <img src={DropIcon} alt="drop-icon" class="drop-icon"></img>
+          <img src={DropIcon} alt="drop-icon" className="drop-icon"></img>
         </div>
       </button>
       <button className={isOpen ? "dropdown open" : "dropdown"}>
-        <div class="logout-title">
-          <img src={LogoutIcon} alt="user-icon" class="logout-icon"></img>
+        <div className="logout-title">
+          <img src={LogoutIcon} alt="user-icon" className="logout-icon"></img>
           <p>خروج</p>
         </div>
       </button>
