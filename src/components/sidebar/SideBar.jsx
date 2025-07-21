46c9ebeca430f7e2b912687f35600c854886eebe
amirparsaro <amirparsarouhani@gmail.com>
2025-07-21 08:32:37 +0330

    change selector color on click

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -3,15 +3,24 @@ import SpeechIcon from "../../assets/images/speech-icon.svg";
 import ArchiveIcon from "../../assets/images/archive-Icon.svg";
 import Title from "./Title";
 import Selector from "./Selector";
+import { useLocation } from "react-router-dom";
 
 const SideBar = () => {
+  const location = useLocation();
+  let pathStartsWith = "";
+  if (location.pathname.startsWith("/convert")) {
+    pathStartsWith = "convert";
+  } else if (location.pathname.startsWith("/archive")) {
+    pathStartsWith = "archive";
+  }
+
   return (
     <aside className="sidebar">
       <Title />
       
       <div className="selector-container">
-        <Selector title="تبدیل گفتار" icon={SpeechIcon} to="/"/>
-        <Selector title="آرشیو" icon={ArchiveIcon} to="/archive"/>
+        <Selector title="تبدیل گفتار" icon={SpeechIcon} to="/" selected={pathStartsWith === "convert"} />
+        <Selector title="آرشیو" icon={ArchiveIcon} to="/archive" selected={pathStartsWith === "archive"} />
       </div>
     </aside>
   );
