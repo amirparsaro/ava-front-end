acde4e65c76144da39280b1c4af8cd81bb186934
amirparsaro <amirparsarouhani@gmail.com>
2025-07-21 09:14:27 +0330

    create upload file functionality

--- src/components/main-content/convert/UploadButton.jsx
+++ src/components/main-content/convert/UploadButton.jsx
@@ -1,12 +1,36 @@
 import "../../../App.css";
+import { useRef } from "react";
 import lightUploadIcon from "../../../assets/images/upload-Icon-white.svg";
 
-const RecordButton = () => {
-    return (
-        <button class="upload-file-button">
-            <img src={lightUploadIcon} alt="upload-file-button"></img>
-        </button>
-    );
+const UploadButton = () => {
+  const fileInputRef = useRef(null);
+
+  const handleClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      console.log(file);
+    }
+  };
+
+  return (
+    <div>
+      <button class="upload-file-button" onClick={handleClick}>
+        <img src={lightUploadIcon} alt="upload-file-button"></img>
+      </button>
+
+      <input
+        type="file"
+        accept="audio/*"
+        ref={fileInputRef}
+        style={{ display: "none" }}
+        onChange={handleFileChange}
+      />
+    </div>
+  );
 };
 
-export default RecordButton;
+export default UploadButton;
